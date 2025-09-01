9f10600250ead9e5db6fac271d620c31fcae7451
Mahmoud shorafa <m2shorafa@gmail.com>
2025-09-01 16:46:07 +0200

    fix the type

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -31,7 +31,10 @@ function PostsPage() {
     const getCookie = (name: string) => {
       const value = `; ${document.cookie}`;
       const parts = value.split(`; ${name}=`);
-      if (parts.length === 2) return parts.pop().split(";").shift();
+      if (parts.length === 2) {
+        const part = parts.pop();
+        return part ? part.split(";").shift() : undefined;
+      }
     };
 
     setToken(getCookie("token"));
